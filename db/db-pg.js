7115cb0fd551d94e25948d569269a11f5da11e3a
Andy Quinche <laqm_14@hotmail.com>
2022-10-14 23:23:34 -0500

    add .env file

--- db/db-pg.js
+++ db/db-pg.js
@@ -1,10 +1,17 @@
 const Pool = require('pg').Pool
+const { //aqui ubicamos las variables que se creo en el archivo donde se puso las variables de entorno
+  DB_USER,
+  DB_PASS,
+  DB_HOST,
+  DB_DBNAME,
+  DB_PORT
+  }=process.env
 const conexion = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'quichuashimi',
-  password: 'root',
-  port: 5432,
+  user: `${DB_USER}`,
+  host: `${DB_HOST}`,
+  database: `${DB_DBNAME}`,
+  password: `${DB_PASS}`,
+  port: `${DB_PORT}`,
 })
 
 module.exports={
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const db = require('./lib/queries')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 app.use(cors())
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
+
 
 app.get('/palabras',db.getPalabras);
 app.get('/voice_recognition',db.getVoice);
