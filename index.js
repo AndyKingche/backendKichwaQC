53e13ea77d3ce18dc1e4ae49577c32128c4ed82a
Andy Quinche <laqm_14@hotmail.com>
2022-10-18 23:06:55 -0500

    transalate words CRUD

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.port || 3000
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
 
-app.get('/palabras',db.getPalabras);
-app.get('/voice_recognition',db.getVoice);
-app.get('/voice_recognition/:nombre',db.getVoiceKichua);
+app.get('/palabras',db.getPalabras)
+app.get('/voice_recognition',db.getVoice)
+app.get('/voice_recognition/:nombre',db.getVoiceKichua)
+app.get('/translate',db.getTraductor)
+app.get('/translate/:id',db.getTraductorId)
+app.post('/translate',db.createTraductor)
+app.put('/translate/:id',db.updateTraductor)
+app.delete('/translate/:id',db.deleteTraductor)
+
 
   app.listen(port, () => {
     console.log(`App running on port http://localhost:${port}`)
